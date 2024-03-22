2fef3572bc567b7cca81f2edabb7d8300c6994d2
johnLamberts <faithsacredoo3@gmail.com>
2024-03-22 12:07:58 +0800

    feat: remove unncessary and unused declarations

--- src/components/user-header.tsx
+++ src/components/user-header.tsx
@@ -1,33 +1,29 @@
-import { useLocation } from "react-router-dom";
 import ModeToggle from "./mode-toggle";
-import TopNav from "./top-nav";
 import UserNav from "./user-nav";
 
-const topNav = [
-  {
-    title: "Overview",
-    href: "dashboard/overview",
-    isActive: true,
-  },
-  {
-    title: "Customers",
-    href: "dashboard/customers",
-    isActive: false,
-  },
-  {
-    title: "Products",
-    href: "dashboard/products",
-    isActive: false,
-  },
-  {
-    title: "Settings",
-    href: "dashboard/settings",
-    isActive: false,
-  },
-];
+// const topNav = [
+//   {
+//     title: "Overview",
+//     href: "dashboard/overview",
+//     isActive: true,
+//   },
+//   {
+//     title: "Customers",
+//     href: "dashboard/customers",
+//     isActive: false,
+//   },
+//   {
+//     title: "Products",
+//     href: "dashboard/products",
+//     isActive: false,
+//   },
+//   {
+//     title: "Settings",
+//     href: "dashboard/settings",
+//     isActive: false,
+//   },
+// ];
 const UserHeader = ({ headerName }: { headerName?: string }) => {
-  const { pathname } = useLocation();
-
   return (
     <>
       {/* {pathname.includes("admin-dashboard") && <TopNav links={topNav} />} */}
